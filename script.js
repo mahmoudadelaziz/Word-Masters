eebc13e778ad2fd6324defcb2228d9d2e437f4a1
mahmoudadelaziz <mahmoudaziz501@gmail.com>
2023-03-23 23:42:40 +0200

    Restructuring the script to apply to all six rows, allowing players to make multiple guesses

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-var isAlpha = function(ch){
-    // This function checks if the input is a valid letter
-    return /^[A-Za-z]$/i.test(ch);
-  }
+var isAlpha = function (ch) {
+  // This function checks if the input is a valid letter
+  return /^[A-Za-z]$/i.test(ch);
+};
 
 row1 = document.getElementById("row1");
 letter1 = document.getElementById("letter1");
 
 let currentLetter = 0;
-let arr = [];
+let lettersEntered = [];
+let playerGuesses = [];
+let wordGuessed = "";
 
 for (let i = 1; i < 6; i++) {
   currentLetter = document.getElementById(`letter${i}`);
   currentLetter.addEventListener("keyup", (event) => {
     if (isAlpha(event.key)) {
-    //   console.log(event.key); // debugging
-      arr.push(event.key);
+      //   console.log(event.key); // debugging
+      lettersEntered.push(event.key);
       document.getElementById(`letter${i + 1}`).focus();
     }
   });
@@ -22,5 +24,6 @@ for (let i = 1; i < 6; i++) {
 
 row1.addEventListener("submit", (event) => {
   event.preventDefault();
-  console.log(arr.join(''))
+  wordGuessed = lettersEntered.join("");
+  console.log(wordGuessed);
 });
