13c0ad3af63e32a8a0995f0d1fc747369067123c
mahmoudadelaziz <mahmoudaziz501@gmail.com>
2023-03-25 02:00:04 +0200

    Version 1.0

--- getRandomWord.js
+++
@@ -1,5 +0,0 @@
-fetch("https://words.dev-apis.com/word-of-the-day")
-  .then((Response) => Response.json())
-  .then((json) => {
-    console.log(json.word);
-  });
--- isAlpha.js
+++
@@ -1,6 +0,0 @@
-var isAlpha = function (ch) {
-  // This function checks if the input is a valid letter
-  return /^[A-Za-z]$/i.test(ch);
-};
-
-export default isAlpha;
